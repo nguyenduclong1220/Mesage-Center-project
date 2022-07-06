b2eccc3bce66f646ad5e4a3448f459b8c8e091b2
Nguyễn Đức Long <69302724+nguyenduclong1220@users.noreply.github.com>
2022-07-06 13:30:06 +0700

    add string,c string.h

---
+++ string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "string.h"
+
+ // trim \n
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) {
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
+
+void str_overwrite_stdout() {
+    printf("\r%s", "> ");
+    fflush(stdout);
+}
+
+
+
+
